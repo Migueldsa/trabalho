701f100de9ca67aed7e6686c4172e087ee60a8ee
Migueldsa <migueldasilvaalves2011@gmail.com>
2021-06-26 16:26:09 -0300

    formulario inserir medico

--- frmInsMedicos.php
+++ frmInsMedicos.php
@@ -21,31 +21,31 @@
         <h3>Adicionar Novo Médico</h3>
     </div>
     <div class="row">
-        <form action="insPaciente.php" method="POST" id="frmInsCompetidor" class="col s12">
+        <form action="insMedico.php" method="POST" id="frmInsMedico" class="col s12">
             <div class="input-field col s8">
             <i class="material-icons prefix">account_circle</i>
-                <label for="lblNome">Informe o Nome do Paciente</label>
+                <label for="lblNome">Informe o Nome do Médico</label>
                 <input type="text" class="form-control" id="txtNome" name="txtNome">
             </div>
             <div class="input-field col s5">
             <i class="material-icons prefix">face</i>
-                <label for="lblIdade">Informe a Idade: </label>
-                <input type="text" class="form-control" id="txtIdade" name="txtIdade">
+                <label for="lblarea">Informe a Área de Atuação: </label>
+                <input type="text" class="form-control" id="txtArea" name="txtArea">
             </div>      
             <div class="input-field col s5">
             <i class="material-icons prefix">mode_edit</i>
-                <label for="lblSintomas">Informe os Sintomas : </label>
-                <input type="text" class="form-control" id="txtSintomas" name="txtSintomas">
+                <label for="lblEscala">Informe os dias de Escala : </label>
+                <input type="text" class="form-control" id="txtEscala" name="txtEscala">
             </div>
             <div class="input-field col s5">
             <i class="material-icons prefix">place</i>
-                <label for="lblEndereco">Informe o Endereço: </label>
-                <input type="text" class="form-control" id="txtEndereco" name="txtEndereco">
+                <label for="lblEmail">Informe o email: </label>
+                <input type="text" class="form-control" id="txtEmail" name="txtEmail">
             </div>      
             <div class="input-field col s5">
             <i class="material-icons prefix">phone</i>
-                <label for="lblTelefone">Informe o Telefone : </label>
-                <input type="text"  class="form-control" id="txtTelefone" name="txtTelefone">
+                <label for="lblCrm">Informe o CRM : </label>
+                <input type="text"  class="form-control" id="txtCrm" name="txtCrm">
             </div>
 
             <br>
@@ -59,7 +59,7 @@
                </button>
 
                <button class="btn waves-effect waves-light indigo" type="button" name="btnVoltar"
-                onclick="JavaScript:location.href='listaPacientes.php'">
+                onclick="JavaScript:location.href='listaMedicos.php'">
                Voltar <i class="material-icons">arrow_back</i> 
                </button>
             </div>
